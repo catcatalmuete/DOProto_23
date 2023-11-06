d30d5494ace3b8e1b3cf5e60043477d1bd73ce43
Eileen Twimasi <eat7129@nyu.edu>
2023-11-05 23:16:15 -0500

    added testing for user sign up

---
+++ data/users.py
@@ -0,0 +1,29 @@
+"""
+This module interfaces to our user data.
+"""
+USERNAME = "username"
+PASSWORD = "password"
+MIN_USER_NAME_LEN = 8
+MIN_PASSWORD_LEN = 8
+
+def get_users():
+    users = {
+        
+		"user_one": {
+            USERNAME: "test_username_1", 
+            PASSWORD: "12345"   
+		},
+        
+        "user_two": {
+            USERNAME: "test_username_2", 
+            PASSWORD: "12345"      
+		},
+        "user_three": {
+            USERNAME: "test_username_3", 
+            PASSWORD: "12345"   
+		},
+        
+	}
+    
+	
+    return users
