b35da24f6d16b09b16d063d3c9096817f306f496
Jeffrey Rathgeber Jr <jr5349@nyu.edu>
2023-12-15 15:56:01 -0500

    all methods for follower collection with tests, added tests for shopping cart methods

--- data/get_followers.py
+++ data/get_followers.py
@@ -3,7 +3,15 @@ import data.db_connect as dbc
 FOLLOWERS_COLLECT = "followers"
 USER_ID = "username"
 
-def get_product():
+def get_followers():
 	dbc.connect_db()
-	return dbc.fetch_all_as_dict(USER_ID, FOLLOWERS_COLLECT)
+	
+	try:
+		if USER_ID not in dbc.get_collection_names():
+			raise ValueError("Username not found in followers collection")
+		
+		return dbc.fetch_all_as_dict(USER_ID, FOLLOWERS_COLLECT)
+	except Exception as e:
+		print(f"Error: {e}")
+
 
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -25,4 +25,56 @@ def test_add_product():
 	}
     response = app.post('/add_product', json=data)
     assert response.status_code == 201
-    
+
+# followers endpoint tests 
+def test_followers_get():
+    app = endpoints.app.test_client()
+    response = app.get('/followers')
+    assert response.status_code == 200
+    assert response.get_json() == {'followers': []}
+
+def test_followers_post():
+    app = endpoints.app.test_client()
+    data = {
+        'user_id': 123,
+        'follower_id': 456
+    }
+    response = app.post('/followers', json=data)
+    assert response.status_code == 201
+    assert response.get_json() == {'message': 'Follower added successfully'}
+
+def test_followers_delete():
+    app = endpoints.app.test_client()
+    response = app.delete('/followers')
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Followers deleted successfully'}
+
+
+# shoppinbg cart endpoints test
+def test_shopping_cart_get():
+    app = endpoints.app.test_client()
+    response = app.get('/shopping_cart')
+    assert response.status_code == 200
+    assert response.get_json() == {'shopping_cart': []}
+
+def test_shopping_cart_post():
+    app = endpoints.app.test_client()
+    data = {
+        'user_id': 123,
+        'product_id': 456
+    }
+    response = app.post('/shopping_cart', json=data)
+    assert response.status_code == 201
+    assert response.get_json() == {'message': 'Product added to shopping cart successfully'}
+
+def test_shopping_cart_delete():
+    app = endpoints.app.test_client()
+    response = app.delete('/shopping_cart')
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Shopping cart cleared successfully'}
+
+def test_shopping_cart_calc_checkout_price():
+    app = endpoints.app.test_client()
+    response = app.get('/shopping_cart/calc_checkout_price')
+    assert response.status_code == 200
+    assert response.get_json() == {'checkout_price': 0.0}
