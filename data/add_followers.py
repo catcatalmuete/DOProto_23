27df7373f72b95c6b56fcfdc8861c121b2c657f1
Eileen Twimasi <eat7129@nyu.edu>
2024-01-30 13:06:21 -0500

    fixed merge conflicts on local computer

--- data/add_followers.py
+++ data/add_followers.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.join(current_dir, '..')
-sys.path.append(parent_dir)
-
-import data.db_connect as dbc
-
-FOLLOWERS_COLLECT = "followers"
-USER_ID = "username"
-FOLLOWERS = 'following'
-
-
-def add_followers(user, follower):
-	data.dbc.connect_db()
-
-	# check if follower is duplicate
-	check = dbc.fetch_one({USER_ID: follower}, FOLLOWERS_COLLECT)
-	if check:
-		return False
-
-	# Insert new follower into database
-	new_follower = {
-		USER_ID: user,
-		FOLLOWERS: follower
-		}
-	dbc.insert_one(new_follower, FOLLOWERS_COLLECT)
-	return True
-=======
 import data.db_connect as dbc
 
 FOLLOWERS_COLLECT = "followers"
@@ -72,4 +41,3 @@ def add_followers(user, followers):
 	_id = dbc.insert_one(FOLLOWERS_COLLECT, new_follower)
 	return _id is not None
 	
->>>>>>> 188c666b1016cb52492c7feb015f099203c70057
--- data/get_followers.py
+++ data/get_followers.py
@@ -1,37 +1,10 @@
-<<<<<<< HEAD
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-parent_dir = os.path.join(current_dir, '..')
-sys.path.append(parent_dir)
-
-=======
->>>>>>> 188c666b1016cb52492c7feb015f099203c70057
 import data.db_connect as dbc
 
 FOLLOWERS_COLLECT = "followers"
 USERNAME = "username"
 
-def get_product():
-	dbc.connect_db()
-	return dbc.fetch_all_as_dict(USER_ID, FOLLOWERS_COLLECT)
-
 def get_followers():
 	dbc.connect_db()
-<<<<<<< HEAD
-
-	try:
-		if USER_ID not in dbc.get_collection_names():
-			raise ValueError("Username not found in followers collection")
-
-		return dbc.fetch_all_as_dict(USER_ID, FOLLOWERS_COLLECT)
-	except Exception as e:
-		print(f"Error: {e}")
-
-=======
 	return dbc.fetch_all_as_dict(USERNAME, FOLLOWERS_COLLECT)
 
 
->>>>>>> 188c666b1016cb52492c7feb015f099203c70057
