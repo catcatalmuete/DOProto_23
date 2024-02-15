229818a19fa615b83babc7eb82a985d2dec5212d
Eileen Twimasi <eat7129@nyu.edu>
2024-02-15 14:16:36 -0500

    updated MongoClient string

--- data/db_connect.py
+++ data/db_connect.py
@@ -29,9 +29,15 @@ def connect_db():
                 raise ValueError('You must set your password '
                                  + 'to use Mongo in the cloud.')
             print("Connecting to Mongo in the cloud.")
+            # "mongodb+srv://eileent7129:<password>@doproto.gdknfwd.mongodb.net/?retryWrites=true&w=majority"
             client = pm.MongoClient(f'mongodb+srv://eileent7129:{password}'
                                     + '@doproto.gdknfwd.mongodb.net/' 
-                                    + '?retryWrites=true&w=majority')
+                                    + '?retryWrites=true'
+                                    + '&w=majority'
+                                    + '&connectTimeoutMS=30000'
+                                    + '&socketTimeoutMS=360000'
+                                    + '&connect=false'
+                                    + '&maxPoolsize=1')
             # PA recommends these settings:
             # + 'connectTimeoutMS=30000&'
             # + 'socketTimeoutMS=None
