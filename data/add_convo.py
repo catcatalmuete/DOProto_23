054e89938023a4832ec8e9188c2a10a6555aacc5
Jeffrey Rathgeber Jr <70925870+JeffreyRathgeber@users.noreply.github.com>
2024-03-17 16:51:41 -0400

    updates to add_convo and update convo file

--- data/add_convo.py
+++ data/add_convo.py
@@ -10,14 +10,25 @@ MESSAGES = "messages"
 TIME_UPDATED = "updated"
 
 # get 2 usernames as strings
-dbc.connect_db()
-user1 = dbc.fetch_one(users, {'_id': USER_ID_1})['username']
-user2 = dbc.fetch_one(users, {'_id': USER_ID_2})['username']
+def add_convo(user1 : str, user2 : str):
+    dbc.connect_db()
 
-# confirm user1 follows user2 before allowing conv oto be started
-if user1 not in gf.get_followers(user2):
-    raise ValueError("User 1 does not follow user 2")
-else:
-    current_utc_time = datetime.utcnow()
-    dbc.insert_one(MESSAGES_COLLECT, {USER_ID_1: user1, USER_ID_2: user2, MESSAGES: [], TIME_UPDATED: current_utc_time})
+    user1id = dbc.fetch_one(users, {'username': user1})['_id']
+    user2id = dbc.fetch_one(users, {'username': user2})['_id']
+
+    # confirm user1 follows user2 before allowing convo to be started
+    if user1 not in gf.get_followers(user2):
+        raise ValueError("User 1 does not follow user 2")
+
+    # check if convo already exists
+    convo_exists = dbc.fetch_one(MESSAGES_COLLECT, {USER_ID_1: user1id, USER_ID_2: user2id})
     
+    if convo_exists:
+        dbc.close_db()
+        return False
+    
+    else:
+        current_utc_time = datetime.utcnow()
+        dbc.insert_one(MESSAGES_COLLECT, {USER_ID_1: user1, USER_ID_2: user2, MESSAGES: [], TIME_UPDATED: current_utc_time})
+        dbc.close_db()
+        return True
