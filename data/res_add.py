232f3d463e45bf0974e4282a9df5c1ec4bc77b1c
Cat Almuete <ca2318@nyu.edu>
2024-05-08 19:55:54 -0400

    Fixed: res_add.py & update_convo.py

--- data/res_add.py
+++ data/res_add.py
@@ -6,26 +6,30 @@ ADDRESS = "address"
 RES_HALL_ID = "_id"
 RES_HALL_COLLECT = "Residence Halls"
 
+
 def add_hall(res_hall: str, address: str):
     dbc.connect_db()
-    found_user = dbc.fetch_one(RES_HALL_COLLECT,{RES_HALL: res_hall})
+    found_user = dbc.fetch_one(RES_HALL_COLLECT, {RES_HALL: res_hall})
     if found_user:
         raise ValueError(f'This residence hall already exists: {res_hall}')
     new_res = {}
     new_res[RES_HALL] = res_hall
     new_res[ADDRESS] = address
-    
+
     _id = dbc.insert_one(RES_HALL_COLLECT, new_res)
     return _id is not None
+
 
 def get_all_res_add() -> dict:
     dbc.connect_db()
     resAdds = dbc.fetch_all_as_dict(RES_HALL, RES_HALL_COLLECT)
     return resAdds
 
+
 def get_res_add(res_hall: str):
     dbc.connect_db()
     return dbc.fetch_one(RES_HALL_COLLECT, {RES_HALL: res_hall})
+
 
 def delete_res_hall(res_id: str):
     res_id = ObjectId(res_id)
--- data/update_convo.py
+++ data/update_convo.py
@@ -8,10 +8,13 @@ USER_ID_2 = "userid2"
 MESSAGES = "messages"
 TIME_UPDATED = "updated"
 
-def update_convo(user1 : str, user2 : str, message : str):
+
+def update_convo(user1: str, user2: str, message: str):
     dbc.connect_db()
     user1id = dbc.fetch_one(users, {'username': user1})['_id']
     current_utc_time = datetime.utcnow()
-    dbc.update_one(MESSAGES_COLLECT, {"$push": {MESSAGES: [user1id, message, current_utc_time]}})
+    dbc.update_one((MESSAGES_COLLECT,
+                    {"$push": {MESSAGES:
+                               [user1id, message, current_utc_time]}}))
     dbc.close_db()
     return True
