6e2dd10c5b327e972e135bdafe6852af534dd2c4
Cat Almuete <ca2318@nyu.edu>
2024-05-08 18:52:41 -0400

    Fixed: add_convo.py

--- data/add_convo.py
+++ data/add_convo.py
@@ -9,8 +9,10 @@ USER_ID_2 = "userid2"
 MESSAGES = "messages"
 TIME_UPDATED = "updated"
 
+
 # get 2 usernames as strings
-def add_convo(user1 : str, user2 : str):
+def add_convo(user1: str, user2: str):
+
     dbc.connect_db()
 
     user1id = dbc.fetch_one(users, {'username': user1})['_id']
@@ -21,14 +23,17 @@ def add_convo(user1 : str, user2 : str):
         raise ValueError("User 1 does not follow user 2")
 
     # check if convo already exists
-    convo_exists = dbc.fetch_one(MESSAGES_COLLECT, {USER_ID_1: user1id, USER_ID_2: user2id})
-    
+    convo_exists = (dbc.fetch_one(MESSAGES_COLLECT,
+                    {USER_ID_1: user1id, USER_ID_2: user2id}))
+
     if convo_exists:
         dbc.close_db()
         return False
-    
+
     else:
         current_utc_time = datetime.utcnow()
-        dbc.insert_one(MESSAGES_COLLECT, {USER_ID_1: user1, USER_ID_2: user2, MESSAGES: [], TIME_UPDATED: current_utc_time})
+        dbc.insert_one((MESSAGES_COLLECT, {USER_ID_1: user1,
+                                           USER_ID_2: user2, MESSAGES: [],
+                                           TIME_UPDATED: current_utc_time}))
         dbc.close_db()
         return True
