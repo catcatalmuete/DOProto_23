644df3830d8e1624f0ac0f1a772292b86b806406
sunny-soeui <sj3048@nyu.edu>
2025-01-04 08:15:20 -0500

    init

--- data/get_followers.py
+++
@@ -1,9 +0,0 @@
-import data.db_connect as dbc
-
-FOLLOWERS_COLLECT = "followers"
-USERNAME = "username"
-
-
-def get_followers():
-    dbc.connect_db()
-    return dbc.fetch_all_as_dict(USERNAME, FOLLOWERS_COLLECT)
--- nyu-swap/src/NYUSwap.js
+++
@@ -1,5 +0,0 @@
-import styles from './UI.module.css'
-
-export default function NYUSwap(props) {
-
-}
